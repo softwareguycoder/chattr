a152406ce3de720c5e8cbaacfd3a6b902a7cf402
softwareguycoder <softwareguycoder@gmail.com>
2019-03-21 08:45:26 -0400

    Added logging to FindClientBySocket function

--- server/src/server.c
+++ server/src/server.c
@@ -44,15 +44,45 @@ int server_socket = 0;
 int is_execution_over = 0;
 
 BOOL FindClientBySocket(void* pClientSocketFd, void* pClientStruct) {
-	if (pClientSocketFd == NULL || pClientStruct == NULL)
+	log_info("In FindClientBySocket");
+
+	log_info("FindClientBySocket: Checking whether both parameters are filled in...");
+
+	if (pClientSocketFd == NULL || pClientStruct == NULL){
+		log_warning("FindClientBySocket: One or both parameters not specified.");
+
+		log_info("FindClientBySocket: Returning FALSE.");
+
+		log_info("FindClientBySocket: Done.");
+
 		return FALSE;
+	}
+
+	log_info("FindClientBySocket: Attempting to retrieve client socket file descriptor...");
 
 	int clientSockFd = *((int*) pClientSocketFd);
+
+	log_debug("FindClientBySocket: clientSockFd = %d", clientSockFd);
+
+	log_info("FindClientBySocket: Attempting to retrieve client structure pointer...");
+
 	CLIENTSTRUCT* client_Struct = (CLIENTSTRUCT*) pClientStruct;
 
+	log_info("FindClientBySocket: Checking whether the client socket is associated with the client structure...");
+
 	if (clientSockFd == client_Struct->sockFD) {
+		log_info("FindClientBySocket: Client structure matching the supplied socket value found.");
+
+		log_info("FindClientBySocket: Returning TRUE.");
+
+		log_info("FindClientBySocket: Done.");
+
 		return TRUE;
 	}
+
+	log_info("FindClientBySocket: Returning FALSE.");
+
+	log_info("FindClientBySocket: Done.");
 
 	return FALSE;
 }
