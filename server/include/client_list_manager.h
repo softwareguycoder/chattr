dae3cb376a164477641be7af858f61d7028641ef
softwareguycoder <softwareguycoder@gmail.com>
2019-04-03 07:38:15 -0400

    Created client_list_manager.h

---
+++ server/include/client_list_manager.h
@@ -0,0 +1,13 @@
+/*
+ * client_list_manager.h
+ *
+ *  Created on: Apr 3, 2019
+ *      Author: bhart
+ */
+
+#ifndef __CLIENT_LIST_MANAGER_H__
+#define __CLIENT_LIST_MANAGER_H__
+
+// TODO: Add declarations here for functions that manage access to the list of clients
+
+#endif /* __CLIENT_LIST_MANAGER_H__ */
