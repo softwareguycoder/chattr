65f5bebc377b8c4e98398e82fe0219f00719a4f3
softwareguycoder <softwareguycoder@gmail.com>
2019-04-05 11:43:40 -0400

    Updated software title

--- client/include/client.h
+++ client/include/client.h
@@ -23,8 +23,8 @@
 #define MAX_LINE_LENGTH 255     // The maximum length of a line
 
 /* key string constants */
-#define SOFTWARE_TITLE  	"TCP echo client v1.0\n"
-#define COPYRIGHT_MESSAGE	"Copyright (c) 2018 by Brian Hart.\n\n"
+#define SOFTWARE_TITLE  	"chattr v1.0 Chat Client\n"
+#define COPYRIGHT_MESSAGE	"Copyright (c) 2019 by Brian Hart.\n\n"
 #define USAGE_STRING	"Usage: client <host name or IP> <port_num>\n" 	// Usage string
 
 #define RECV_BLOCK_SIZE	1
