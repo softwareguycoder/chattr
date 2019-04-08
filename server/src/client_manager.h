d7d5eea244e119e9ebc2b3f2593a7a326a5fc365
softwareguycoder <softwareguycoder@gmail.com>
2019-04-08 12:30:52 -0400

    Created client_manager.h

---
+++ server/src/client_manager.h
@@ -0,0 +1,13 @@
+/*
+ * client_manager.h
+ *
+ *  Created on: Apr 8, 2019
+ *      Author: bhart
+ */
+
+#ifndef SRC_CLIENT_MANAGER_H_
+#define SRC_CLIENT_MANAGER_H_
+
+
+
+#endif /* SRC_CLIENT_MANAGER_H_ */
