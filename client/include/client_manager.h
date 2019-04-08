e3d3803ea3ed3cdab2e803e2815e7e2d0787f758
softwareguycoder <softwareguycoder@gmail.com>
2019-04-08 07:52:08 -0400

    Created client_manager.c/.h

---
+++ client/include/client_manager.h
@@ -0,0 +1,13 @@
+/*
+ * client_manager.h
+ *
+ *  Created on: Apr 8, 2019
+ *      Author: bhart
+ */
+
+#ifndef __CLIENT_MANAGER_H__
+#define __CLIENT_MANAGER_H__
+
+
+
+#endif /* __CLIENT_MANAGER_H__ */
