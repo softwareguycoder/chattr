4d86ed57d53be0b9e1fa0b880b1eb089098ecde2
softwareguycoder <softwareguycoder@gmail.com>
2019-04-09 13:56:12 -0400

    Added logging to the CreateSendQueueEntry function

--- client/src/send_queue_entry.c
+++ client/src/send_queue_entry.c
@@ -11,29 +11,70 @@
 #include "send_queue_entry.h"
 
 LPSENDQUEUEENTRY CreateSendQueueEntry(const char* message) {
-	if (message == 0 || message[0] == '\0') {
+	log_debug("In CreateSendQueueEntry");
+
+	log_info(
+			"CreateSendQueueEntry: Checking whether the message passed is NULL or blank...");
+
+	if (message == NULL || message[0] == '\0') {
+		log_error("CreateSendQueueEntry: Message passed was blank.  Stopping.");
+
+		log_debug("CreateSendQueueEntry: Done.");
+
 		CleanupClient(ERROR);
 	}
+
+	log_info("CreateSendQueueEntry: The message passed is non-blank.");
+
+	log_info(
+			"CreateSendQueueEntry: Checking whether the message is longer than %d chars...",
+			MAX_MESSAGE_LEN - 1);
 
 	/* max length of message, including newline, is MAX_MESSAGE_LEN */
 	if (MAX_MESSAGE_LEN < strlen(message)) {
-		fprintf(stderr, "CreateSendQueueEntry: Message must be %d characters or fewer in length.",
-				MAX_MESSAGE_LEN - 1);
+		log_error(
+				"CreateSendQueueEntry: Message must be %d characters or fewer in length.");
+
+		if (stderr != get_error_log_file_handle()) {
+			fprintf(stderr,
+					"CreateSendQueueEntry: Message must be %d characters or fewer in length.",
+					MAX_MESSAGE_LEN - 1);
+		}
+
+		log_debug("CreateSendQueueEntry: Done.");
 
 		CleanupClient(ERROR);
 	}
 
-	LPSENDQUEUEENTRY result = (LPSENDQUEUEENTRY)malloc(1*sizeof(SENDQUEUEENTRY));
+	log_info("CreateSendQueueEntry: The message is of a valid length.");
+
+	log_info(
+			"CreateSendQueueEntry: Allocating memory for a new SENDQUEUEENTRY structure instance...");
+
+	LPSENDQUEUEENTRY result = (LPSENDQUEUEENTRY) malloc(
+			1 * sizeof(SENDQUEUEENTRY));
 	if (result == NULL) {
+		log_error(
+				"CreateSendQueueEntry: Failed to allocate sufficient memory for a new SENDQUEUEENTRY instance...");
+
+		log_debug("CreateSendQueueEntry: Done.");
+
 		CleanupClient(ERROR);
 	}
+
+	log_info(
+			"CreateSendQueueEntry: Successfully allocated memory for a new SENDQUEUEENTRY structure instance.");
+
+	log_info(
+			"CreateSendQueueEntry: Initializing the new SENDQUEUEENTRY structure instance with message information...");
 
 	uuid_generate(result->id);	// give this entry a unique identifier
 
 	// Set all the time fields in this structure to the same value to start with.
 	// This way, they all have valid values.  When the message is actually queued, and then
 	// delivered, at these points, the relevant fields will be updated further.
-	result->createDateTime = result->deliveredDateTime = result->queuedDateTime = time(NULL);
+	result->createDateTime = result->deliveredDateTime =
+			result->queuedDateTime = time(NULL);
 	result->status = CREATED;
 
 	/* put the value in the 'message' parameter into the message member of the
@@ -43,34 +84,39 @@ LPSENDQUEUEENTRY CreateSendQueueEntry(const char* message) {
 	char uuid_value[37];
 	uuid_unparse(result->id, uuid_value);
 
-	log_info("CreateSendQueueEntry: Created send queue entry '%s'.", uuid_value);
+	log_info("CreateSendQueueEntry: Created send queue entry '%s'.",
+			uuid_value);
+
+	log_debug("CreateSendQueueEntry: Done.");
 
 	return result;
 }
 
-void FreeSendQueueEntry(void* pSendQueueEntry){
+void FreeSendQueueEntry(void* pSendQueueEntry) {
 	log_debug("In FreeSendQueueEntry");
 
-		log_info(
-				"FreeSendQueueEntry: Checking whether supplied SENDQUEUEENTRY pointer is NULL...");
+	log_info(
+			"FreeSendQueueEntry: Checking whether supplied SENDQUEUEENTRY pointer is NULL...");
 
-		if (pSendQueueEntry == NULL) {
-			log_warning(
-					"FreeSendQueueEntry: The send queue entry structure has already been freed.  Nothing to do.");
-
-			log_debug("FreeSendQueueEntry: Done.");
-
-			return;
-		}
-
-		log_info("FreeSendQueueEntry: The pSendQueueEntry pointer references a valid memory address.");
-
-		log_info("FreeSendQueueEntry: Freeing the SENDQUEUEENTRY pointer...");
-
-		free(pSendQueueEntry);
-		pSendQueueEntry = NULL;
-
-		log_info("FreeSendQueueEntry: The memory has been released back to the system.");
+	if (pSendQueueEntry == NULL) {
+		log_warning(
+				"FreeSendQueueEntry: The send queue entry structure has already been freed.  Nothing to do.");
 
 		log_debug("FreeSendQueueEntry: Done.");
+
+		return;
+	}
+
+	log_info(
+			"FreeSendQueueEntry: The pSendQueueEntry pointer references a valid memory address.");
+
+	log_info("FreeSendQueueEntry: Freeing the SENDQUEUEENTRY pointer...");
+
+	free(pSendQueueEntry);
+	pSendQueueEntry = NULL;
+
+	log_info(
+			"FreeSendQueueEntry: The memory has been released back to the system.");
+
+	log_debug("FreeSendQueueEntry: Done.");
 }
