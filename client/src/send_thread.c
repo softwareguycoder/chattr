4445f43141564e6bcff1fe6db0bd46509096d727
softwareguycoder <softwareguycoder@gmail.com>
2019-04-10 08:51:04 -0400

    Introducing Hungarian notation

--- client/src/send_thread.c
+++ client/src/send_thread.c
@@ -15,10 +15,10 @@ HTHREAD hSendThread;
 ///////////////////////////////////////////////////////////////////////////////
 // ShouldKeepSending function
 
-BOOL ShouldKeepSending(const char* cur_line) {
-	if (cur_line == NULL || cur_line[0] == '\0'
-			|| strcasecmp(cur_line, PROTOCOL_QUIT_COMMAND) == 0
-			|| strcmp(cur_line, MSG_TERMINATOR) == 0) {
+BOOL ShouldKeepSending(const char* pszCurLine) {
+	if (pszCurLine == NULL || pszCurLine[0] == '\0'
+			|| strcasecmp(pszCurLine, PROTOCOL_QUIT_COMMAND) == 0
+			|| strcmp(pszCurLine, MSG_TERMINATOR) == 0) {
 		return FALSE;
 	}
 
@@ -35,24 +35,19 @@ void ShowPrompt() {
 ///////////////////////////////////////////////////////////////////////////////
 // SendThread function
 
-void *SendThread(void *pData) {
+void *SendThread(void *pvData) {
 	// Check if NULL was passed for the user state; if this is so, stop.
-	if (pData == NULL) {
+	if (pvData == NULL) {
 		return NULL;
 	}
-
-	// Try to get the socket file descriptor of the client connection from
-	// the user state.
-	int *pClientSockFd = (int*) pData;
-	int client_socket = *pClientSockFd;
 
 	// Double check to ensure we have a valid socket file descriptor for
 	// communications.  If not, then stop.
-	if (!IsSocketValid(client_socket)) {
+	if (!IsSocketValid(nClientSocket)) {
 		return NULL;
 	}
 
-	char cur_line[MAX_LINE_LENGTH + 1]; // Buffer for the current line inputted by the user
+	char szCurLine[MAX_LINE_LENGTH + 1]; // Buffer for the current line inputted by the user
 
 	//ShowPrompt();
 
@@ -60,14 +55,14 @@ void *SendThread(void *pData) {
 	// and not the entire program, we can call fgets here and lines sent from the chat
 	// server will still be received by the other thread we have spun up for receiving text
 	// and writing it to stdout.
-	while (NULL != fgets(cur_line, MAX_LINE_LENGTH, stdin)) {
-		if (cur_line[0] == '\n') {
+	while (NULL != fgets(szCurLine, MAX_LINE_LENGTH, stdin)) {
+		if (szCurLine[0] == '\n') {
 			continue;		// skip instances where the user just presses ENTER.
 		}
 
 		// If we are here, then there is something to be sent.  Go ahead and send it to
 		// the socket.
-		if (0 > Send(client_socket, cur_line)) {
+		if (0 > Send(nClientSocket, szCurLine)) {
 			// If we are here, then an error occurred with sending.
 			continue;
 		}
@@ -76,7 +71,7 @@ void *SendThread(void *pData) {
 
 		// Ask if we should keep sending, or whether it's time to stop waiting for
 		// input.
-		if (!ShouldKeepSending(cur_line))
+		if (!ShouldKeepSending(szCurLine))
 			break;
 
 		//ShowPrompt();
