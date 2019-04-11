34211a9c70c4d826ad0d77515f48d0a558378681
softwareguycoder <softwareguycoder@gmail.com>
2019-04-11 15:42:11 -0400

    Stripping logging

--- server/src/server_functions.c
+++ server/src/server_functions.c
@@ -23,50 +23,19 @@ BOOL CheckCommandLineArgs(int argc, char *argv[]) {
 // threads in an orderly way
 
 void CleanupServer(int nExitCode) {
-	LogDebug("In CleanupServer");
-
-	LogDebug("CleanupServer: exitCode = %d", nExitCode);
-
 	// Handle the case where the user presses CTRL+C in the terminal
 	// by performing an orderly shut down of the server and freeing
 	// operating system resources.
 
-	LogInfo("CleanupServer: Obtaining a lock on the client list mutex...");
-
 	LockMutex(g_hClientListMutex);
 	{
-		LogInfo("CleanupServer: Lock established.");
-
-		LogInfo(
-				"CleanupServer: Checking whether the count of connected clients is greater than zero...");
-
 		if (g_nClientCount > 0) {
-			LogInfo(
-					"CleanupServer: The count of connected clients is greater than zero.");
-
-			LogInfo("CleanupServer: Forcibly disconnecting each client...");
-
 			ForEach(&g_pClientList, DisconnectClient);
-
-			LogInfo("CleanupServer: Disconnection operation completed.");
-		} else {
-			LogInfo(
-					"CleanupServer: Zero clients are currently connected.  Nothing to do.");
 		}
-
-		LogInfo("CleanupServer: Releasing the client list lock...");
 	}
 	UnlockMutex(g_hClientListMutex);
 
-	LogInfo("CleanupServer: Client list lock released.");
-
-	LogInfo("CleanupServer: Calling QuitServer...");
-
 	QuitServer();
-
-	LogInfo("CleanupServer: Finished calling QuitServer.");
-
-	LogInfo("CleanupServer: Closing the log file...");
 
 	CloseLogFileHandles();
 
