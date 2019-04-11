0f5219f58189a87ea9b8782d56c0fd45b112a7c4
softwareguycoder <softwareguycoder@gmail.com>
2019-04-11 12:36:43 -0400

    Added docs for the ForEach function

--- server/include/client_list.h
+++ server/include/client_list.h
@@ -19,7 +19,15 @@ POSITION* FindMember(POSITION** listHead, void* value,
 int RemoveElement(POSITION** listHead, void* value,
 		LPCOMPARE_ROUTINE lpfnSearch);
 void DestroyList(POSITION** listHead, LPDEALLOC_ROUTINE lpfnDeallocFunc);
+
+/**
+ * @brief Executes an action for each member of a non-empty list.
+ * @param listHead Reference to the head node of the list.
+ * @param lpfnForEachRoutine Reference to the function to be executed for each
+ * element.  This function is passed a reference to the element.
+ */
 void ForEach(POSITION** listHead, LPACTION_ROUTINE lpfnForEachRoutine);
+
 POSITION* GetNext(POSITION* pos);
 
 #endif /* __CLIENT_LIST_H__*/
