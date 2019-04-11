1cd3dd8544fc918f0f3d9d1688cce4ff27747cde
softwareguycoder <softwareguycoder@gmail.com>
2019-04-11 11:36:30 -0400

    Added docs for CleanupServer function

--- server/include/server_functions.h
+++ server/include/server_functions.h
@@ -8,7 +8,12 @@
 #ifndef __SERVER_FUNCTIONS_H__
 #define __SERVER_FUNCTIONS_H__
 
+/**
+ * @brief Frees resources consumed by the server and exits the application with the specified code.
+ * @param nExitCode Exit code to supply to the operating system when this program is terminated.
+ */
 void CleanupServer(int nExitCode);
+
 void ConfigureLogFile();
 void CreateClientListMutex();
 void DestroyClientListMutex();
