322f0ae84800bbd811036d90d05173db8c1985b3
softwareguycoder <softwareguycoder@gmail.com>
2019-04-16 13:16:16 -0400

    Added documentation for the function signatures and global variable

--- server/include/client_list.h
+++ server/include/client_list.h
@@ -6,19 +6,55 @@
 #include "root.h"
 #include "position.h"
 
+/**
+ * @brief Global variable containing the count of elements in the list.
+ * @remarks Always use a mutex when accessing this variable in a multi-
+ * threaded application.
+ */
 extern int g_nClientCount;
 
+/**
+ * @brief Callback to be used to compare two elements of the linked list.
+ * @param pKey Address of a memory location that holds the search key.
+ * @param pData Address of the current element in the list.
+ * @returns TRUE if the current element of the list has information matching
+ * the search key; FALSE otherwise.
+ * @remarks Use this as the function signature for a comparison routine whose
+ * address is to be provided to the FindElement function.
+ */
 typedef BOOL (*LPCOMPARE_ROUTINE)(void*, void*);
+
+/**
+ * @brief Callback to be used to deallocate the storage occupied by the
+ * current element of the list.
+ * @param pData Pointer to the memory storage occupied by the current list
+ * element.
+ * @remarks Provide the address of a function matching the signature of this
+ * routine to the DestroyList function.  The function referenced will be called
+ * once for each element in the list. */
 typedef void (*LPDEALLOC_ROUTINE)(void*);
 
-/* Callback that is called for everyone in the list */
+/**
+ * @brief Callback that does an action for the current item in the list.
+ * @param pData Address of the current element in the list.
+ * @remarks Use this as the function signature for a function whose address
+ * is to be provided to the ForEach function that will specify the actions
+ * to be executed for each element in the list.
+ */
 typedef void (*LPACTION_ROUTINE)(void*);
 
+/**
+ * @brief Searches the list for an element containing data that matches the
+ * search key, and then returns the address of the POSITION structure that
+ * addresses the element's position.
+ * @param
+ */
 POSITION* FindElement(POSITION** listHead, void* value,
-		LPCOMPARE_ROUTINE lpfnCompare);
+        LPCOMPARE_ROUTINE lpfnCompare);
 int RemoveElement(POSITION** listHead, void* value,
-		LPCOMPARE_ROUTINE lpfnSearch);
-void DestroyList(POSITION** listHead, LPDEALLOC_ROUTINE lpfnDeallocFunc);
+        LPCOMPARE_ROUTINE lpfnSearch);
+void DestroyList(POSITION** listHead,
+    LPDEALLOC_ROUTINE lpfnDeallocFunc);
 
 /**
  * @brief Executes an action for each member of a non-empty list.
