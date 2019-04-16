a7a659b3eb13016e48fcf7a11c6c1cda693e3358
softwareguycoder <softwareguycoder@gmail.com>
2019-04-16 13:09:18 -0400

    Added documentation for the POSITION structure.

--- server/include/position.h
+++ server/include/position.h
@@ -8,6 +8,10 @@
 #ifndef __POSITION_H__
 #define __POSITION_H__
 
+/**
+ * @brief Defines a node of the linked list and also serves to locate the
+ * node.
+ */
 typedef struct _tagPOSITION {
     ROOT* pListRoot;
 
