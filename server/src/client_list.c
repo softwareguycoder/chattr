42178992cba4187e7ff3dab5cd6d1f477ca15194
softwareguycoder <softwareguycoder@gmail.com>
2019-04-16 13:40:54 -0400

    Added documentation to the AddHead and AddTail functions.

--- server/src/client_list.c
+++ server/src/client_list.c
@@ -22,25 +22,39 @@ POSITION* AddHead(void* pvData) {
         HandleError(INVALID_LIST_DATA);
     }
 
+    /* Create a new node to serve as the new head. */
 	POSITION* pListHead = (POSITION*) calloc(1, sizeof(POSITION));
 	if (pListHead == NULL) {
 	    HandleError(FAILED_ALLOC_HEAD);
 	}
 
+	/* Create a new root structure to bear information about the
+	 * head and tail. */
 	ROOT* pListRoot = (ROOT*) calloc(1, sizeof(ROOT));
 	if (pListRoot == NULL) {
 		HandleError(FAILED_ALLOC_ROOT);
 	}
 
+	/* Set the root structure to point to the newly-created node as
+	 * both the head and the tail (since there is just one node in the
+	 * list right now). */
 	pListRoot->pHead = pListHead;
 	pListRoot->pTail = pListHead;
 
+	/* Set the pListRoot of the newly-created head element to reference
+	 * back to the root. */
 	pListHead->pListRoot = pListRoot;
+
+	/* Since this is the first element, there is nothing for the pNext and
+	 * pPrev pointers to reference right now. */
 	pListHead->pNext = NULL;
 	pListHead->pPrev = NULL;
 
+	/* Initialize the pvData member of the new node with the data
+	 * pointer passed to this function. */
 	pListHead->pvData = pvData;
 
+	/* Return the address of the newly-created node. */
 	return pListHead;
 }
 
@@ -61,14 +75,29 @@ BOOL AddTail(POSITION** ppListHead, void* pvData) {
 	    HandleError(FAILED_ALLOC_NEW_NODE);
 	}
 
+	/* Set the pListRoot member of the new node to point at
+	 * the address referenced by the list head */
 	pos->pListRoot = (*ppListHead)->pListRoot;
+
+	/* Set the pNext pointer of the new node to NULL since this node
+	 * is going on the tail of the list. */
 	pos->pNext = NULL;
+
+	/* Set the pPrev pointer of the new node to the address of the current
+	 * list tail since this new node is now the tail. */
 	pos->pPrev = (*ppListHead)->pListRoot->pTail;
+
+	/* Set the new node to reference the data passed to this function. */
 	pos->pvData = pvData;
 
+	/* Set the (current) tail of the linked list to point at this
+	 * node as the next element after it. */
 	(*ppListHead)->pListRoot->pTail->pNext = pos;
+
+	/* Set the newly-created node to be the new tail of the list. */
 	(*ppListHead)->pListRoot->pTail = pos;
 
+	/* Return success. */
 	return TRUE;
 }
 
