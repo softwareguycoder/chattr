390e1d5ec5e4298cd2b57b8578e80b02648e9dbb
softwareguycoder <softwareguycoder@gmail.com>
2019-04-26 14:02:34 -0400

    Create server_exceptions.h

---
+++ server/include/server_exceptions.h
@@ -0,0 +1,13 @@
+/*
+ * server_exceptions.h
+ *
+ *  Created on: Apr 26, 2019
+ *      Author: bhart
+ */
+
+#ifndef __SERVER_EXCEPTIONS_H__
+#define __SERVER_EXCEPTIONS_H__
+
+void ThrowInvalidPointerException();
+
+#endif /* INCLUDE_SERVER_EXCEPTIONS_H_ */
