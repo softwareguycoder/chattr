139886f4a6f9485e6bd2f3ef31a2a8296875f689
softwareguycoder <softwareguycoder@gmail.com>
2019-04-26 14:05:33 -0400

    Create ThrowInvalidPointerException function

---
+++ server/src/server_exceptions.c
@@ -0,0 +1,15 @@
+// server_exceptions.c - Implementations of the functions found in
+// server_exceptions.h that display errors on the screen and then clean up
+// the server
+//
+
+#include "stdafx.h"
+#include "server.h"
+
+#include "server_exceptions.h"
+#include "server_functions.h"
+
+void ThrowInvalidPointerException() {
+    fprintf(stderr, ERROR_INVALID_PTR_ARG);
+    CleanupServer(ERROR);
+}
