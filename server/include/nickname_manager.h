0c9c1f463a168e7734778a459d35a2ad35998d8b
softwareguycoder <softwareguycoder@gmail.com>
2019-04-29 08:23:53 -0400

    Added nickname_manager.c/.h

---
+++ server/include/nickname_manager.h
@@ -0,0 +1,12 @@
+// nickname_manager.h - Defines the interface for a set of functions that
+// allow us to manage the nicknames in use by this chat server's various
+// clients.
+
+#ifndef __NICKNAME_MANAGER_H__
+#define __NICKNAME_MANAGER_H__
+
+/**
+ * @brief Fills the specified buffer with
+ */
+
+#endif /* __NICKNAME_MANAGER_H__ */
