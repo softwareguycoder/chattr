077155a88f20771d6ce9d9abc823ea126756b784
softwareguycoder <softwareguycoder@gmail.com>
2019-04-29 08:27:05 -0400

    Moved GetNicknameFromClient function to nickname_manager.c/.h

--- server/include/client_thread_functions.h
+++ server/include/client_thread_functions.h
@@ -68,17 +68,6 @@ BOOL EndChatSession(LPCLIENTSTRUCT lpSendingClient);
 int GetConnectedClientCount();
 
 /**
- * @brief Parses the user's chosen nickname.  Really just tokenizes src on
- * spaces and returns the second token (ostensibly, the value after the NICK
- * command).
- * @param dest Address of a buffer to receive the result.
- * @param src Address of a buffer containing the source (input).
- * @remarks src may not contain a string literal.  It should be the address
- * of a char buffer that has had the value to be parsed literally copied into
- * it, say, with strcpy().
- */
-void GetNicknameFromClient(char* dest, char* src);
-/**
  * @brief Extracts the address of a CLIENTSTRUCT instance from the user state
  * bag passed to the client thread.
  * @param pvClientThreadUserState Address of a storage location containing user
--- server/src/client_thread_functions.c
+++ server/src/client_thread_functions.c
@@ -235,41 +235,6 @@ int GetConnectedClientCount() {
     return nResult;
 }
 
-///////////////////////////////////////////////////////////////////////////////
-// GetNickname function
-
-void GetNicknameFromClient(char* dest, char* src) {
-    if (IsNullOrWhiteSpace(src)) {
-        ThrowNullReferenceException();
-    }
-
-    if (dest == NULL) {
-        ThrowNullReferenceException();
-    }
-
-    const int BUFFER_SIZE = strlen(src);
-
-    char szNickname[BUFFER_SIZE + 1];
-    memset(szNickname, 0, BUFFER_SIZE);
-
-    char *pszBuffer = strtok(src, " ");
-    if (pszBuffer != NULL) {
-        pszBuffer = strtok(NULL, " ");
-    }
-
-    if (!IsNullOrWhiteSpace(pszBuffer)) {
-        /* we expect that the buffer now contains a nickname
-         * such as brian\n and we expect the contents to end
-         * in a newline.  Since we parsed on whitespace as a
-         * separator, make sure to do a Trim() on the string
-         * so as to remove any other remaining whitespace
-         * characters. */
-
-        Trim(szNickname, BUFFER_SIZE + 1, pszBuffer);
-
-        strcpy(dest, szNickname);
-    }
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 // GetSendingClientInfo function
