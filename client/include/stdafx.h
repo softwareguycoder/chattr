8f1584f08184f2dbdbe051e6706ce2c167bd2025
softwareguycoder <softwareguycoder@gmail.com>
2019-05-06 13:56:39 -0400

    Breaking BOOL type definition into the api_core library

--- client/include/stdafx.h
+++ client/include/stdafx.h
@@ -19,12 +19,13 @@
 // Note: you need to do a apt-get install uuid-dev to build this code!
 #include <uuid/uuid.h>
 
-#include <../../inetsock_core/inetsock_core/include/inetsock_core.h>
-#include <../../conversion_core/conversion_core/include/conversion_core.h>
-#include <../../debug_core/debug_core/include/debug_core.h>
-#include <../../console_core/console_core/include/console_core.h>
+#include <../../../api_core/api_core/include/api_core.h>
+#include <../../../common_core/common_core/include/common_core.h>
+#include <../../../conversion_core/conversion_core/include/conversion_core.h>
+#include <../../../console_core/console_core/include/console_core.h>
+#include <../../../inetsock_core/inetsock_core/include/inetsock_core.h>
+#include <../../../debug_core/debug_core/include/debug_core.h>
 #include <../../../threading_core/threading_core/include/threading_core.h>
 #include <../../../mutex_core/mutex_core/include/mutex_core.h>
-#include <../../../common_core/common_core/include/common_core.h>
 
 #endif//__STDAFX_H__
